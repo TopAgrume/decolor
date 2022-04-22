d70fde54491908487a5d2eba572f1db1c0a815e0
Rabouille Mathis <mathis.rabouille@epita.fr>
2022-04-22 17:49:52 +0200

    débug du fichier gtk.c + ajout fonctions update et tracking souris(WIP)

--- GTK/gtk.c
+++ GTK/gtk.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "gtk.h"
 #include <stdlib.h>
+#include <SDL/SDL.h>
+#include "SDL/SDL_image.h"
 
 GdkRGBA* color;
 GtkColorButton* ColorButton;
@@ -14,21 +16,22 @@ GtkRadioButton* segment;
 GtkRadioButton* square;
 GtkRadioButton* triangle;
 GtkRadioButton* circle;
-GtkGrid* toolsgrid;
-GtkButton* previous;
-GtkButton* next;
+GtkWidget* toolsgrid;
+GtkWidget* previous;
+GtkWidget* next;
 GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_SAVE;
-GtkWidget *SaveButton;
+GtkButton *SaveButton;
 GtkWidget *dialog;
 GtkWidget *image;
-GtkWidget *FileChooser;
+GtkFileChooser *FileChooser;
 GtkFileChooser *chooser;
 GtkScale* scale;
 unsigned char scale_nb = 0;
 GtkComboBoxText* filtres;
 GtkButton* apply;
+GtkWidget* eventbox;
 
-GSList* tools = g_list_alloc();
+GSList* tools;
 int tool_value = -1;
 
 
@@ -48,7 +51,7 @@ gboolean on_next(GtkButton *self, gpointer user_data);
 gboolean on_FileChoosing_file_set(GtkFileChooserButton *f, gpointer user_data);
 gboolean on_SaveButton_clicked(GtkButton *f, gpointer user_data);
 gboolean on_apply_clicked(GtkButton *self, gpointer user_data);
-
+static gboolean mouse_moved(GtkWidget *widget,GdkEvent *event, gpointer user_data);
 
 int main(int argc, char *argv[])
 {
@@ -56,34 +59,37 @@ int main(int argc, char *argv[])
 
     GtkBuilder* Builder = gtk_builder_new_from_file("GUI.glade");
 
+    tools = g_slist_alloc();
     // Getting objects
     window = GTK_WIDGET(gtk_builder_get_object(Builder, "MyWindow"));
-    ColorButton = gtk_builder_get_object(Builder, "Color");
+    ColorButton = GTK_COLOR_BUTTON(gtk_builder_get_object(Builder, "Color"));
     image = GTK_WIDGET(gtk_image_new());
-    toolsgrid = gtk_builder_get_object(Builder, "toolsgrid");
+    toolsgrid = GTK_WIDGET(gtk_builder_get_object(Builder, "toolsgrid"));
     dialog = gtk_file_chooser_dialog_new("Save File", GTK_WINDOW(window), action,
     "_Cancel", GTK_RESPONSE_CANCEL, "_Save", GTK_RESPONSE_ACCEPT, NULL);
-    SaveButton = GTK_WIDGET(gtk_builder_get_object(Builder, "Save"));
-    FileChooser = GTK_WIDGET(gtk_builder_get_object(Builder, "FileChooser"));
+    SaveButton = GTK_BUTTON(gtk_builder_get_object(Builder, "Save"));
+    FileChooser = GTK_FILE_CHOOSER(gtk_builder_get_object(Builder, "FileChooser"));
+    eventbox = gtk_event_box_new ();
+    gtk_container_add(GTK_CONTAINER (eventbox), image);
     // Tools buttons
-    brush = gtk_builder_get_object(Builder, "brush");
+    brush = GTK_RADIO_BUTTON(gtk_builder_get_object(Builder, "brush"));
     gtk_radio_button_set_group (brush ,tools);
-    bucket = gtk_builder_get_object(Builder, "bucket"); 
-    eraser = gtk_builder_get_object(Builder, "eraser");
-    bigeraser = gtk_builder_get_object(Builder, "bigeraser");
-    segment = gtk_builder_get_object(Builder, "segment");
-    square = gtk_builder_get_object(Builder, "square");
-    triangle = gtk_builder_get_object(Builder, "triangle");
-    circle = gtk_builder_get_object(Builder, "circle");
-    set_tools_group(toolsgrid, brush);
+    bucket = GTK_RADIO_BUTTON(gtk_builder_get_object(Builder, "bucket")); 
+    eraser = GTK_RADIO_BUTTON(gtk_builder_get_object(Builder, "eraser"));
+    bigeraser = GTK_RADIO_BUTTON(gtk_builder_get_object(Builder, "bigeraser"));
+    segment = GTK_RADIO_BUTTON(gtk_builder_get_object(Builder, "segment"));
+    square = GTK_RADIO_BUTTON(gtk_builder_get_object(Builder, "square"));
+    triangle = GTK_RADIO_BUTTON(gtk_builder_get_object(Builder, "triangle"));
+    circle = GTK_RADIO_BUTTON(gtk_builder_get_object(Builder, "circle"));
+    //set_tools_group(toolsgrid, brush);
     scale = GTK_SCALE(gtk_builder_get_object(Builder, "scale"));
     scale_nb = gtk_range_get_value(&(scale->range));
     printf("Scale number : %c", scale_nb);
     filtres = GTK_COMBO_BOX_TEXT(gtk_builder_get_object(Builder, "Filtres"));
     apply = GTK_BUTTON(gtk_builder_get_object(Builder, "Appliquer"));
     // Previous and Next
-    previous = gtk_builder_get_object(Builder, "previous");
-    next = gtk_builder_get_object(Builder, "next");
+    previous = GTK_WIDGET(gtk_builder_get_object(Builder, "previous"));
+    next = GTK_WIDGET(gtk_builder_get_object(Builder, "next"));
 
     chooser = GTK_FILE_CHOOSER(dialog);
     gtk_file_chooser_set_current_name (chooser, "Saving Image");
@@ -106,7 +112,8 @@ int main(int argc, char *argv[])
     g_signal_connect(SaveButton, "clicked", G_CALLBACK(on_SaveButton_clicked), image);
     g_signal_connect(FileChooser, "file-set", G_CALLBACK(on_FileChoosing_file_set), image);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    
+    g_signal_connect (G_OBJECT (eventbox), "motion-notify-event",G_CALLBACK (mouse_moved), NULL);    
+    gtk_widget_set_events(eventbox, GDK_POINTER_MOTION_MASK);
 
     gtk_widget_show_all(window);
 
@@ -115,72 +122,125 @@ int main(int argc, char *argv[])
     return EXIT_SUCCESS;
 }
 
+void update(SDL_Surface* src)
+{
+    SDL_SaveBMP(src, "tmpfile.bmp");
+    GdkPixbuf *pixbuf;
+
+    pixbuf = gdk_pixbuf_new_from_file("tmpfile.bmp", NULL);
+    
+    gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
+
+    if (pixbuf)
+        g_object_unref(pixbuf);
+
+    gtk_widget_show(image);
+}
+
+static gboolean mouse_moved(GtkWidget *widget,GdkEvent *event, gpointer user_data) 
+{
+    if (event->type==GDK_MOTION_NOTIFY && widget != NULL && user_data == NULL) 
+    {
+        GdkEventMotion* e =(GdkEventMotion*)event;
+        printf("Coordinates: (%u,%u)\n", (guint)e->x,(guint)e->y);
+    }
+    return FALSE;
+}
+
 gboolean on_previous(GtkButton* self, gpointer user_data)
 {
     //use this fonction to revert last modification
+    if (self && user_data)
+        return FALSE;
     return FALSE;
 }
 
 gboolean on_next(GtkButton* self, gpointer user_data)
 {
     //use this fonction to re-do last modification that was reverted
+    if (self && user_data)
+        return FALSE;  
     return FALSE;
 }
 
 gboolean on_brush(GtkRadioButton *self, gpointer user_data)
 {
-    if (user_data == NULL && gtk_toggle_button_get_active(self) == TRUE)
+    if (user_data == NULL &&
+        gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(self)) == TRUE)
+    {
         tool_value = 1;
+    }
 
     return FALSE;
 }
 
 gboolean on_bucket(GtkRadioButton *self, gpointer user_data)
 {
-     if (user_data == NULL && gtk_toggle_button_get_active(self) == TRUE)
+     if (user_data == NULL &&
+         gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(self)) == TRUE)
+     {
          tool_value = 2;
+     }
      return FALSE;
 }
 
 gboolean on_eraser(GtkRadioButton *self, gpointer user_data)
 {
-     if (user_data == NULL && gtk_toggle_button_get_active(self) == TRUE)
+     if (user_data == NULL &&
+         gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(self)) == TRUE)
+     {
          tool_value = 3;
+     }
      return FALSE;
 }
 
 gboolean on_bigeraser(GtkRadioButton *self, gpointer user_data)
 {
-     if (user_data == NULL && gtk_toggle_button_get_active(self) == TRUE)
+     if (user_data == NULL &&
+         gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(self)) == TRUE)
+     {
          tool_value = 4;
+     }
      return FALSE;
 }
 
 gboolean on_segment(GtkRadioButton *self, gpointer user_data)
 {
-     if (user_data == NULL && gtk_toggle_button_get_active(self) == TRUE)
+     if (user_data == NULL &&
+         gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(self)) == TRUE)
+     {
          tool_value = 5;
+     }
      return FALSE;
 }
 
 gboolean on_square(GtkRadioButton *self, gpointer user_data)
 {
-     if (user_data == NULL && gtk_toggle_button_get_active(self) == TRUE)
+     if (user_data == NULL &&
+         gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(self)) == TRUE)
+     {
          tool_value = 6;
+     }
      return FALSE;
 }
 
 gboolean on_triangle(GtkRadioButton *self, gpointer user_data)
 {
-     if (user_data == NULL && gtk_toggle_button_get_active(self) == TRUE)
+     if (user_data == NULL &&
+         gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(self)) == TRUE)
+     {
          tool_value = 7;
+     }
      return FALSE;
 }
 
 gboolean on_circle(GtkRadioButton *self, gpointer user_data)
 {
-     if (user_data == NULL && gtk_toggle_button_get_active(self) == TRUE)
+     if (user_data == NULL &&
+         gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(self)) == TRUE)
+     {
          tool_value = 8;
+     }
      return FALSE;
 }
 
@@ -190,7 +250,7 @@ void set_tools_group(GtkGrid* toolsgrid, GtkRadioButton* brush)
     {
         for (int col = 1-row; col < 4; col++)
         {
-            gtk_radio_button_join_group(gtk_grid_get_child_at (toolsgrid,col,row),
+            gtk_radio_button_join_group(GTK_RADIO_BUTTON(gtk_grid_get_child_at(toolsgrid,col,row)),
             brush);
         }
     }
@@ -231,7 +291,7 @@ gboolean on_FileChoosing_file_set(GtkFileChooserButton *f, gpointer user_data)
     char *filename;
 
     filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(f));
-    pixbuf = gdk_pixbuf_new_from_file_at_size(filename, 750,750,NULL);
+    pixbuf = gdk_pixbuf_new_from_file(filename, NULL);
     g_free(filename);
 
     gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
@@ -245,6 +305,10 @@ gboolean on_FileChoosing_file_set(GtkFileChooserButton *f, gpointer user_data)
 
 gboolean on_Color_set(GtkColorButton *self, gpointer user_data)
 {
+
+    if (user_data != NULL)
+        return FALSE;
+
     gtk_color_button_get_rgba(self,color);
 
     return FALSE;
@@ -253,6 +317,11 @@ gboolean on_Color_set(GtkColorButton *self, gpointer user_data)
 
 gboolean on_apply_clicked(GtkButton *self, gpointer user_data)
 {
+    // Just for avoiding warnings you can change if you want
+    if (self == NULL && user_data != NULL)
+        return FALSE;
+
+
     char* fil = gtk_combo_box_text_get_active_text(filtres);
 
     switch(fil[0])
